ba0e92a18629937e7ecd5f25c03d2f4b3e4bcada
Eric <eric.diep@beyondedge.co>
2023-12-11 19:53:54 +0700

    Update report

--- fe-ndstore-adminpage/src/apis/statistic.api.js
+++ fe-ndstore-adminpage/src/apis/statistic.api.js
@@ -5,7 +5,7 @@ class StatisticApis {
         if (!fromDay || !toDay) {
             return;
         }
-        return httpClientApi.httpGet(`api/admin/manage/statistical/sale/total/?from=${fromDay}&to=${toDay}&type=${type}`)
+        return httpClientApi.httpGet(`api/admin/manage/report/order/sale/total/?from=${fromDay}&to=${toDay}&type=${type}`)
     }
 
     async getReportProductSales(fromDay, toDay) {
